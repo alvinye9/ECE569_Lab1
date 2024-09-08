8537028706c993f0c55a57f2d07e3499b83fa0a5
Alvin Ye <alvinye9@gmail.com>
2024-09-08 14:10:01 -0400

    step 4 complete

--- Step5/rrbot.launch.py
+++ Step5/rrbot.launch.py
@@ -38,6 +38,6 @@ def generate_launch_description():
         [
             joint_state_publisher_node,
             robot_state_publisher_node,
-            rviz_node,
+            # rviz_node,
         ]
     )
